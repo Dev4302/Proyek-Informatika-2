93c69ea42bafb7becf54acac1709fbc8039b43a4
Ralvin Adrian <itsmesteins@gmail.com>
2024-11-07 13:33:35 +0700

    add basic controller for API

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,33 @@
 
 namespace App\Controllers;
 
+use Google\Cloud\Translate\V2\TranslateClient;
+
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('home');
+        $translate = new TranslateClient([
+            'key' => getenv('GOOGLE_TRANSLATE_API_KEY')
+        ]);
+
+        // Translate text from english to french.
+        $result = $translate->translate('Hello world!', [
+            'target' => 'fr'
+        ]);
+
+        echo $result['text'] . "\n";
+
+        // Detect the language of a string.
+        $result = $translate->detectLanguage('Greetings from Michigan!');
+
+        echo $result['languageCode'] . "\n";
+        
+        // Get all languages supported for translation.
+        $languages = $translate->languages();
+
+        foreach ($languages as $language) {
+            echo $language . "\n";
+        }
     }
 }
